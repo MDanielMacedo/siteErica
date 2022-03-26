5b1f315afb447c2a873776efcec0b48fcfb81f2e
Daniel <mdanielmacedo78@gmail.com>
2022-03-26 19:02:22 -0300

    progresso 40%

--- js/script.js
+++ js/script.js
@@ -1,12 +1,38 @@
-$(document).ready(function () {
-  $('.slider').slick({
-    dots: true,
-    infinite: true,
-    speed: 1000,
-    fade: true,
-    cssEase: 'linear',
-    slidesToShow: 1,
-    adaptiveHeight: true,
-    autoplay: true,
+let time = 4000,
+  currentImageIndex = 0,
+  images = document.querySelectorAll('.slider img'),
+  max = images.length;
+
+
+function nextImage(){
+
+  images[currentImageIndex].classList.remove('selected');
+
+  currentImageIndex++;
+
+  if(currentImageIndex >= max){
+    currentImageIndex = 0;
+  }
+
+  images[currentImageIndex].classList.add('selected');
+}
+
+function start(){
+  setInterval(() => {
+  //troca de imagem
+  nextImage();
+  }, time)
+}
+
+window.addEventListener('load', start);
+
+$(document).ready(function(){
+  var botao = $('.dropdown-toggler');
+  var btnDown = $('.dropdown');
+  botao.on('click', function(event){
+    btnDown.stop(true,true).slideToggle();
+    event.stopPropagation();
   });
 });
+
+
